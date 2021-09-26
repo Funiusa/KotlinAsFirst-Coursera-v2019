81ae7ed34f3a4f73a7b31bde466631b966555d33
Denis Zenukhin <funiusa@gmail.com>
2021-09-27 01:19:57 +0300

    The seconds, lengthInMeters, angleInRadian, trackLength are executed

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -31,7 +31,7 @@ fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c
  * Поиск одного из корней квадратного уравнения
  */
 fun quadraticEquationRoot(a: Double, b: Double, c: Double) =
-    (-b + sqrt(discriminant(a, b, c))) / (2 * a)
+	(-b + sqrt(discriminant(a, b, c))) / (2 * a)
 
 /**
  * Пример
@@ -39,18 +39,29 @@ fun quadraticEquationRoot(a: Double, b: Double, c: Double) =
  * Поиск произведения корней квадратного уравнения
  */
 fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
-    val sd = sqrt(discriminant(a, b, c))
-    val x1 = (-b + sd) / (2 * a)
-    val x2 = (-b - sd) / (2 * a)
-    return x1 * x2 // Результат
+	val sd = sqrt(discriminant(a, b, c))
+	val x1 = (-b + sd) / (2 * a)
+	val x2 = (-b - sd) / (2 * a)
+	return x1 * x2 // Результат
 }
 
 /**
  * Пример главной функции
  */
-fun main() {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+fun main(args: Array<String>) {
+	val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
+	val sec = seconds(8, 0, 0)
+	//println("Seconds: ${args[1]}")
+	val len = lengthInMeters(8, 2,11)
+	val rad = angleInRadian(36, 14, 35)
+	val track = trackLength(3.0, 0.0, 0.0, 4.0)
+	val third = thirdDigit(3801)
+	println("Root product: $x1x2")
+	println("Seconds: $sec")
+	println("Length in Meters: %.2f".format(len))
+	println("angle In Radian: %.5f".format(rad))
+	println("track lenght: $track")
+	println("Third digit: $third")
 }
 
 /**
@@ -59,7 +70,7 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = (hours * 60 * 60) + (minutes * 60) + seconds
 
 /**
  * Тривиальная
@@ -68,7 +79,12 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+	val versh = 4.445
+	val arsh = 16 * versh
+	val sag = 3 * arsh
+	return ((sagenes * sag) + (arshins * arsh) + vershoks * versh) / 100
+}
 
 /**
  * Тривиальная
@@ -76,7 +92,8 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double =
+	(deg / 57.2958) + (min / 3437.75) + (sec / 206265.0001359)
 
 /**
  * Тривиальная
@@ -84,7 +101,8 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double =
+	sqrt(sqr(x2 - x1) + sqr(y2 - y1))
 
 /**
  * Простая
@@ -92,9 +110,116 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Пользователь задает целое число, большее 100 (например, 3801).
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
-fun thirdDigit(number: Int): Int = TODO()
+fun thirdDigit(number: Int): Int = 1
 
-/**
+/**package lesson1.task1
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import kotlin.math.PI
+
+class Tests {
+    @Test
+    @Tag("Example")
+    fun sqr() {
+        assertEquals(0, sqr(0))
+        assertEquals(4, sqr(2))
+        assertEquals(9, sqr(-3))
+    }
+
+    @Test
+    @Tag("Example")
+    fun sqrDouble() {
+        assertEquals(0.0, sqr(0.0), 1e-13)
+        assertEquals(4.0, sqr(2.0), 1e-13)
+        assertEquals(9.0, sqr(-3.0), 1e-13)
+    }
+
+    @Test
+    @Tag("Example")
+    fun discriminant() {
+        assertEquals(0.0, discriminant(0.0, 0.0, 0.0), 1e-13)
+        assertEquals(0.0, discriminant(1.0, -2.0, 1.0), 1e-13)
+        assertEquals(1.0, discriminant(1.0, 3.0, 2.0), 1e-13)
+    }
+
+    @Test
+    @Tag("Example")
+    fun quadraticEquationRoot() {
+        assertEquals(2.0, quadraticEquationRoot(1.0, -3.0, 2.0), 1e-13)
+        assertEquals(1.0, quadraticEquationRoot(1.0, -2.0, 1.0), 1e-13)
+        assertEquals(-3.0, quadraticEquationRoot(1.0, 6.0, 9.0), 1e-13)
+    }
+
+    @Test
+    @Tag("Example")
+    fun quadraticRootProduct() {
+        assertEquals(1.0, quadraticRootProduct(1.0, -2.0, 1.0), 1e-13)
+        assertEquals(9.0, quadraticRootProduct(1.0, 6.0, 9.0), 1e-13)
+        assertEquals(2.0, quadraticRootProduct(1.0, 3.0, 2.0), 1e-13)
+    }
+
+    @Test
+    @Tag("Trivial")
+    fun seconds() {
+        assertEquals(30035, seconds(8, 20, 35))
+        assertEquals(86400, seconds(24, 0, 0))
+        assertEquals(13, seconds(0, 0, 13))
+    }
+
+    @Test
+    @Tag("Trivial")
+    fun lengthInMeters() {
+        assertEquals(18.98, lengthInMeters(8, 2, 11), 1e-2)
+        assertEquals(2.13, lengthInMeters(1, 0, 0), 1e-2)
+    }
+
+    @Test
+    @Tag("Trivial")
+    fun angleInRadian() {
+        assertEquals(0.63256, angleInRadian(36, 14, 35), 1e-5)
+        assertEquals(PI / 2.0, angleInRadian(90, 0, 0), 1e-5)
+    }
+
+    @Test
+    @Tag("Trivial")
+    fun trackLength() {
+        assertEquals(5.0, trackLength(3.0, 0.0, 0.0, 4.0), 1e-5)
+        assertEquals(1.0, trackLength(0.0, 1.0, -1.0, 1.0), 1e-5)
+        assertEquals(1.41, trackLength(1.0, 1.0, 2.0, 2.0), 1e-2)
+    }
+
+    @Test
+    @Tag("Easy")
+    fun thirdDigit() {
+        assertEquals(8, thirdDigit(3801))
+        assertEquals(1, thirdDigit(100))
+        assertEquals(0, thirdDigit(1000))
+    }
+
+    @Test
+    @Tag("Easy")
+    fun travelMinutes() {
+        assertEquals(216, travelMinutes(9, 25, 13, 1))
+        assertEquals(1, travelMinutes(21, 59, 22, 0))
+    }
+
+    @Test
+    @Tag("Easy")
+    fun accountInThreeYears() {
+        assertEquals(133.1, accountInThreeYears(100, 10), 1e-2)
+        assertEquals(1.0, accountInThreeYears(1, 0), 1e-2)
+        assertEquals(104.0, accountInThreeYears(13, 100), 1e-2)
+    }
+
+    @Test
+    @Tag("Easy")
+    fun numberRevert() {
+        assertEquals(874, numberRevert(478))
+        assertEquals(201, numberRevert(102))
+    }
+}
  * Простая
  *
  * Поезд вышел со станции отправления в h1 часов m1 минут (например в 9:25) и
